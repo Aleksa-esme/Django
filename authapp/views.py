9d5caf3c60b3e9bcd54e24eead25d0b32a7601e0
Aleksa-esme <esme1990@mail.ru>
2021-05-05 19:00:04 +0300

    added form and conroller for register

--- authapp/views.py
+++ authapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 
-from authapp.forms import UserLoginForm
+from authapp.forms import UserLoginForm, UserRegisterForm
 
 def login(request):
     if request.method == 'POST':
@@ -19,3 +19,15 @@ def login(request):
         context = {'title': 'Authorizing',
                    'form': form}
     return render(request, 'authapp/login.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegisterForm()
+        context = {'title': 'Registration',
+                   'form': form}
+    return render(request, 'authapp/register.html', context)
