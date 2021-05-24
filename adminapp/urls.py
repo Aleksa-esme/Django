f738500798f74a3cc967f4d0ebe4f7217310e7f2
Aleksa-esme <esme1990@mail.ru>
2021-05-24 16:52:31 +0300

    replacing function to class

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 
-from adminapp.views import index, admin_users_read, admin_users_create, admin_users_update, admin_users_remove, admin_users_restore
+from adminapp.views import index, admin_users_restore
+from adminapp.views import UserListView, UserCreateView, UserUpdateView, UserDeleteView
 from adminapp.views import admin_categories_read, admin_categories_create, admin_categories_update, admin_categories_remove, admin_categories_restore
 
 app_name = 'adminapp'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('admin-users-read/', admin_users_read, name='admin_users_read'),
-    path('admin-users-create/', admin_users_create, name='admin_users_create'),
-    path('admin-users-update/<int:user_id>/', admin_users_update, name='admin_users_update'),
-    path('admin-users-remove/<int:user_id>/', admin_users_remove, name='admin_users_remove'),
+    path('admin-users-read/', UserListView.as_view(), name='admin_users_read'),
+    path('admin-users-create/', UserCreateView.as_view(), name='admin_users_create'),
+    path('admin-users-update/<int:pk>/', UserUpdateView.as_view(), name='admin_users_update'),
+    path('admin-users-remove/<int:pk>/', UserDeleteView.as_view(), name='admin_users_remove'),
     path('admin-users-restore/<int:user_id>/', admin_users_restore, name='admin_users_restore'),
 
     path('admin-categories-read/', admin_categories_read, name='admin_categories_read'),
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from authapp.models import User
 from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm, ProductCategoryEditForm
@@ -11,11 +13,17 @@ from mainapp.models import ProductCategory
 def index(request):
     return render(request, 'adminapp/admin.html')
 
+'''
 @user_passes_test(lambda u: u.is_superuser)
 def admin_users_read(request):
     context = {'users': User.objects.all()}
     return render(request, 'adminapp/admin-users-read.html', context)
+'''
+class UserListView(ListView):
+    model = User
+    template_name = 'adminapp/admin-users-read.html'
 
+'''
 @user_passes_test(lambda u: u.is_superuser)
 def admin_users_create(request):
     if request.method == 'POST':
@@ -27,7 +35,15 @@ def admin_users_create(request):
         form = UserAdminRegisterForm()
     context = {'form': form}
     return render(request, 'adminapp/admin-users-create.html', context)
+'''
+class UserCreateView(CreateView):
+    model = User
+    template_name = 'adminapp/admin-users-create.html'
+    form_class = UserAdminRegisterForm
+    success_url = reverse_lazy('admin_staff:admin_users_read')
 
+
+'''
 @user_passes_test(lambda u: u.is_superuser)
 def admin_users_update(request, user_id):
     selected_user = User.objects.get(id=user_id)
@@ -44,13 +60,29 @@ def admin_users_update(request, user_id):
                }
 
     return render(request, 'adminapp/admin-users-update-delete.html', context)
+'''
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = 'adminapp/admin-users-update-delete.html'
+    success_url = reverse_lazy('admin_staff:admin_users_read')
+    form_class = UserAdminProfileForm
 
+
+'''
 @user_passes_test(lambda u: u.is_superuser)
 def admin_users_remove(request, user_id):
     user = User.objects.get(id=user_id)
     user.is_active = False
     user.save()
     return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
+'''
+class UserDeleteView(DeleteView):
+    model = User
+    template_name = 'adminapp/admin-users-update-delete.html'
+    success_url = reverse_lazy('admin_staff:admin_users_read')
+
+
+
 
 @user_passes_test(lambda u: u.is_superuser)
 def admin_users_restore(request, user_id):
